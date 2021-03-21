4dfea75fe54d59b4fae950f6a719b80a0752e3ba
marcobelo <marco.barone.belo@gmail.com>
2021-03-21 11:21:19 -0300

    refactor: move x and y to Coord and move x, y validation to coord.validate

--- code/board.py
+++ code/board.py
@@ -1,17 +1,30 @@
 from code.exceptions import InvalidMove
 
 
+class Coord:
+    column: int
+    row: int
+
+    def __init__(self, column, row):
+        self.column = column
+        self.row = row
+        self._validate()
+
+    def _validate(self):
+        INVALID_VALUE_FOR_COORD_MSG = "Invalid value for {}, should be one of [0, 1, 2]"
+        if self.column not in [0, 1, 2]:
+            raise InvalidMove(INVALID_VALUE_FOR_COORD_MSG.format("column"))
+        elif self.row not in [0, 1, 2]:
+            raise InvalidMove(INVALID_VALUE_FOR_COORD_MSG.format("row"))
+
+
 class Board:
     def __init__(self):
         self._data = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-    def make_a_move(self, x, y, player_value):
-        self._validate_move(x, y)
-        self._data[y][x] = player_value
+    def make_a_move(self, coord: Coord, player_value: int):
+        self._validate_move(coord)
+        self._data[coord.row][coord.column] = player_value
 
-    def _validate_move(self, x, y):
-        INVALID_VALUE_FOR_COORD_MSG = "Invalid value for {}, should be one of [0, 1, 2]"
-        if x not in [0, 1, 2]:
-            raise InvalidMove(INVALID_VALUE_FOR_COORD_MSG.format("column"))
-        elif y not in [0, 1, 2]:
-            raise InvalidMove(INVALID_VALUE_FOR_COORD_MSG.format("row"))
+    def _validate_move(self, coord: Coord):
+        pass
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,4 +1,4 @@
-from code.board import Board
+from code.board import Board, Coord
 from code.exceptions import InvalidMove
 
 import pytest
@@ -13,17 +13,15 @@ def test_create_new_board():
 def test_make_a_move():
     expected_result = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
     board = Board()
-    board.make_a_move(1, 1, 1)
+    board.make_a_move(Coord(1, 1), 1)
 
     assert board._data == expected_result
 
 
 @pytest.mark.parametrize("column", [-1, 3])
-def test_invalid_column_value_should_raise_InvalidMove_exception(column):
-    board = Board()
+def test_invalid_coord_column_value_should_raise_InvalidMove_exception(column):
     with pytest.raises(InvalidMove) as exception:
-        board.make_a_move(column, 1, 1)
-
+        Coord(column, 1)
     assert (
         exception.value.message
         == "Invalid value for column, should be one of [0, 1, 2]"
@@ -31,11 +29,9 @@ def test_invalid_column_value_should_raise_InvalidMove_exception(column):
 
 
 @pytest.mark.parametrize("row", [-1, 3])
-def test_invalid_row_value_should_raise_InvalidMove_exception(row):
-    board = Board()
+def test_invalid_coord_row_value_should_raise_InvalidMove_exception(row):
     with pytest.raises(InvalidMove) as exception:
-        board.make_a_move(1, row, 1)
-
+        Coord(1, row)
     assert (
         exception.value.message == "Invalid value for row, should be one of [0, 1, 2]"
     )
