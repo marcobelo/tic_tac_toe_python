4d827065baf041b98974de267761ffe033749c80
marcobelo <marco.barone.belo@gmail.com>
2021-03-21 00:57:57 -0300

    test: add validation for range of values for column and row move

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,4 +1,7 @@
 from code.board import Board
+from code.exceptions import InvalidMove
+
+import pytest
 
 
 def test_create_new_board():
@@ -9,8 +12,30 @@ def test_create_new_board():
 
 def test_make_a_move():
     expected_result = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
-    player_value = 1
     board = Board()
-    board.make_a_move(1, 1, player_value)
+    board.make_a_move(1, 1, 1)
 
     assert board == expected_result
+
+
+@pytest.mark.parametrize("column", [-1, 3])
+def test_invalid_column_value_should_raise_InvalidMove_exception(column):
+    board = Board()
+    with pytest.raises(InvalidMove) as exception:
+        board.make_a_move(column, 1, 1)
+
+    assert (
+        exception.value.message
+        == "Invalid value for column, should be one of [0, 1, 2]"
+    )
+
+
+@pytest.mark.parametrize("row", [-1, 3])
+def test_invalid_row_value_should_raise_InvalidMove_exception(row):
+    board = Board()
+    with pytest.raises(InvalidMove) as exception:
+        board.make_a_move(1, row, 1)
+
+    assert (
+        exception.value.message == "Invalid value for row, should be one of [0, 1, 2]"
+    )
