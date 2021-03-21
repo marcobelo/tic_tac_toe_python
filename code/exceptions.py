0f59d8eae17dfeda9fea9b28c74b41e05e298936
marcobelo <marco.barone.belo@gmail.com>
2021-03-21 11:53:46 -0300

    test: move to an occupied coord will raise InvalidMove

--- code/exceptions.py
+++ code/exceptions.py
@@ -2,3 +2,9 @@ class InvalidCoord(Exception):
     def __init__(self, message):
         super().__init__(message)
         self.message = message
+
+
+class InvalidMove(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,8 @@
 from code.board import Board
 from code.coord import Coord
+from code.exceptions import InvalidMove
+
+import pytest
 
 
 def test_create_new_board():
@@ -12,7 +15,6 @@ def test_make_a_move():
     expected_result = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
     board = Board()
     board.make_a_move(Coord(1, 1), 1)
-
     assert board._data == expected_result
 
 
@@ -21,5 +23,12 @@ def test_set_and_get_value():
     board = Board()
     board._set_value(Coord(1, 2), expected_value)
     result_value = board._get_value(Coord(1, 2))
-
     assert result_value == expected_value
+
+
+def test_move_to_an_occupied_coord_raise_InvalidMove_exception():
+    board = Board()
+    board.make_a_move(Coord(1, 1), 1)
+    with pytest.raises(InvalidMove) as exception:
+        board.make_a_move(Coord(1, 1), 1)
+    assert exception.value.message == "Invalid move, this position is already occupied"
