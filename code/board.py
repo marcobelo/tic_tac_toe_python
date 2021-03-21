c4bae7b1b45e4f18e1645cdc97760e5fabb2aef4
marcobelo <marco.barone.belo@gmail.com>
2021-03-20 23:51:14 -0300

    feat: Board can make_a_move

--- code/board.py
+++ code/board.py
@@ -1,3 +1,6 @@
 class Board:
     def __init__(self):
         self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    def make_a_move(self, x, y, player_value):
+        self.board[y][x] = player_value
