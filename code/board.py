1a6d1004121053b81aab58249b47e937cd235c81
marcobelo <marco.barone.belo@gmail.com>
2021-03-21 11:45:12 -0300

    feat: create get and set to access board data

--- code/board.py
+++ code/board.py
@@ -7,7 +7,13 @@ class Board:
 
     def make_a_move(self, coord: Coord, player_value: int):
         self._validate_move(coord)
-        self._data[coord.row][coord.column] = player_value
+        self._set_value(coord, player_value)
 
     def _validate_move(self, coord: Coord):
         pass
+
+    def _set_value(self, coord: Coord, value):
+        self._data[coord.row][coord.column] = value
+
+    def _get_value(self, coord: Coord):
+        return self._data[coord.row][coord.column]
